cada23774c85fb8f0fe1a857b4b82314ce9fe2f6
Mohammed-Salman-Uddin <160523742017@islec.edu.in>
2024-07-06 02:49:05 +0530

    user app creation started

---
+++ src/users/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Profile
+
+class ProfileAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+# Register your models here.
+admin.site.register(Profile,ProfileAdmin)
